4634c1a9c45b1e69818874a85fc1a7c549eacac7
10013160 <10013160@MH0034MSD>
2020-10-14 23:17:00 +0530

    added new controller

---
+++ TechGentle.BookStoreCore/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechGentle.BookStoreCore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "all books";
+        }
+    }
+}
